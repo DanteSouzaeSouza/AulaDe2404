46f6701dbd2cde0d27efcb70a2f1f1176d14d57e
Dante Souza e Souza <lokinmodar@gmail.com>
2020-04-24 20:28:23 -0300

    Commit Inicial

---
+++ app/src/main/java/br/com/theoldpinkeye/aulade2404/MainActivity.java
@@ -0,0 +1,14 @@
+package br.com.theoldpinkeye.aulade2404;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
