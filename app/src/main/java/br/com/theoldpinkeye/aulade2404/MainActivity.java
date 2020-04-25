4fa538256514514dd2ceef4b91277ef718669606
Dante Souza e Souza <lokinmodar@gmail.com>
2020-04-24 21:54:35 -0300

    Trabalhos no Layout

--- app/src/main/java/br/com/theoldpinkeye/aulade2404/MainActivity.java
+++ app/src/main/java/br/com/theoldpinkeye/aulade2404/MainActivity.java
@@ -3,6 +3,7 @@ package br.com.theoldpinkeye.aulade2404;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,57 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(getApplicationContext(),
+                "onCreate() Executado",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(getApplicationContext(),
+                "onStart() Executado",
+                Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(getApplicationContext(),
+                "onStop() Executado",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(getApplicationContext(),
+                "onDestroy() Executado",
+                Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(getApplicationContext(),
+                "onPause() Executado",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(getApplicationContext(),
+                "onResume() Executado",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(getApplicationContext(),
+                "onRestart() Executado",
+                Toast.LENGTH_LONG).show();
     }
 }
